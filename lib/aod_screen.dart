394c2252faa64a2f508a5bcf997d4e7d7750b3a8
ivin-titus <ivintitus@hotmail.com>
2025-01-06 23:40:27 +0530

    Changes on Temp unit and update frequency reflects on weatherwidgets and services, Disabled non-working Options on settings

--- lib/aod_screen.dart
+++ lib/aod_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'dart:ui';
 import '../widgets/weather_widget_mini.dart';
 import 'package:wakelock_plus/wakelock_plus.dart';
 import '../widgets/time_widget.dart';
@@ -10,7 +9,6 @@ import 'navbar.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'widgets/services/settings_service.dart';
 
-
 class AODScreen extends ConsumerStatefulWidget {
   const AODScreen({Key? key}) : super(key: key);
 
@@ -18,132 +16,46 @@ class AODScreen extends ConsumerStatefulWidget {
   ConsumerState<AODScreen> createState() => _AODScreenState();
 }
 
-class _AODScreenState extends ConsumerState<AODScreen>
-    with SingleTickerProviderStateMixin {
+class _AODScreenState extends ConsumerState<AODScreen> {
   bool _showCloseButton = false;
-  bool _isHovered = false;
   DateTime? _lastTapTime;
-  late AnimationController _animationController;
-  late Animation<double> _scaleAnimation;
-
+  
   @override
   void initState() {
     super.initState();
     _checkAODStatus();
-    _initializeAnimations();
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
   }
 
-void _checkAODStatus() async {
-  final isAODEnabled = ref.read(aodEnabledProvider);
-  if (isAODEnabled) {
-    await WakelockPlus.enable();
-    await SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
-  } else {
-    _exitAODScreen();
-  }
-}
-
-  void _initializeAnimations() {
-    _animationController = AnimationController(
-      duration: const Duration(milliseconds: 200),
-      vsync: this,
-    );
-
-    _scaleAnimation = Tween<double>(
-      begin: 0.8,
-      end: 1.0,
-    ).animate(CurvedAnimation(
-      parent: _animationController,
-      curve: Curves.easeOutBack,
-    ));
+  void _checkAODStatus() {
+    if (ref.read(aodEnabledProvider)) {
+      WakelockPlus.enable();
+    }
   }
 
   void _handleTap() {
     final now = DateTime.now();
     if (_lastTapTime != null &&
         now.difference(_lastTapTime!).inMilliseconds < 300) {
-      setState(() {
-        _showCloseButton = true;
-      });
-      _animationController.forward(from: 0.0);
-
-      Future.delayed(const Duration(seconds: 3), () {
-        if (mounted) {
-          setState(() {
-            _showCloseButton = false;
-          });
-        }
-      });
+      setState(() => _showCloseButton = true);
+      Future.delayed(const Duration(seconds: 3), 
+        () => mounted ? setState(() => _showCloseButton = false) : null);
     }
     _lastTapTime = now;
   }
 
   Widget _buildCloseButton() {
-    return AnimatedOpacity(
-      opacity: _showCloseButton ? 1.0 : 0.0,
-      duration: const Duration(milliseconds: 300),
-      curve: Curves.easeInOut,
-      child: _showCloseButton
-          ? Positioned(
-              top: 20,
-              right: 20,
-              child: ScaleTransition(
-                scale: _scaleAnimation,
-                child: MouseRegion(
-                  onEnter: (_) => setState(() => _isHovered = true),
-                  onExit: (_) => setState(() => _isHovered = false),
-                  child: GestureDetector(
-                    onTapDown: (_) => setState(() => _isHovered = true),
-                    onTapUp: (_) => setState(() => _isHovered = false),
-                    onTapCancel: () => setState(() => _isHovered = false),
-                    onTap: () {
-                      _animationController
-                          .reverse()
-                          .then((_) => _exitAODScreen());
-                    },
-                    child: AnimatedContainer(
-                      duration: const Duration(milliseconds: 200),
-                      transform: Matrix4.identity()
-                        ..scale(_isHovered ? 1.1 : 1.0),
-                      child: Container(
-                        padding: const EdgeInsets.all(12),
-                        decoration: BoxDecoration(
-                          color: Colors.black.withOpacity(0.3),
-                          borderRadius: BorderRadius.circular(200),
-                          border: Border.all(
-                            color: Colors.white
-                                .withOpacity(_isHovered ? 0.3 : 0.2),
-                            width: 1,
-                          ),
-                          boxShadow: [
-                            BoxShadow(
-                              color: const Color.fromARGB(255, 27, 27, 27)
-                                  .withOpacity(_isHovered ? 0.4 : 0.3),
-                              blurRadius: _isHovered ? 16 : 12,
-                              spreadRadius: _isHovered ? 3 : 2,
-                            ),
-                          ],
-                        ),
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(20),
-                          child: BackdropFilter(
-                            filter:
-                                ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
-                            child: Icon(
-                              Icons.close_rounded,
-                              color: Colors.white
-                                  .withOpacity(_isHovered ? 0.9 : 0.8),
-                              size: _isHovered ? 30 : 28,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            )
-          : const SizedBox.shrink(),
+    return Visibility(
+      visible: _showCloseButton,
+      child: Positioned(
+        top: 20,
+        right: 20,
+        child: IconButton(
+          icon: const Icon(Icons.close, color: Colors.white, size: 28),
+          onPressed: _exitAODScreen,
+          padding: const EdgeInsets.all(12),
+        ),
+      ),
     );
   }
 
@@ -151,10 +63,9 @@ void _checkAODStatus() async {
     _disableAODFeatures();
     Navigator.of(context).pushReplacement(
       PageRouteBuilder(
-        pageBuilder: (context, animation, secondaryAnimation) => const NavBar(),
-        transitionsBuilder: (context, animation, secondaryAnimation, child) {
-          return FadeTransition(opacity: animation, child: child);
-        },
+        pageBuilder: (_, __, ___) => const NavBar(),
+        transitionsBuilder: (_, animation, __, child) =>
+            FadeTransition(opacity: animation, child: child),
         transitionDuration: const Duration(milliseconds: 200),
       ),
     );
@@ -168,9 +79,7 @@ void _checkAODStatus() async {
     );
   }
 
-  double _getTimeTextSize(double screenWidth, double screenHeight) {
-    final smallerDimension =
-        screenWidth < screenHeight ? screenWidth : screenHeight;
+  double _getTimeTextSize(double smallerDimension) {
     if (smallerDimension < 300) return 48;
     if (smallerDimension < 600) return 90;
     if (smallerDimension < 1200) return 96;
@@ -186,125 +95,79 @@ void _checkAODStatus() async {
     return isWidth ? 0.20 : 0.13;
   }
 
-  double _getResponsiveWidth(double screenWidth, bool isLandscape) {
-    if (screenWidth < 300) return isLandscape ? 0.50 : 0.90;
-    if (screenWidth < 600) return isLandscape ? 0.45 : 0.85;
-    if (screenWidth < 1200) return isLandscape ? 0.42 : 0.80;
-    if (screenWidth < 2000) return isLandscape ? 0.40 : 0.75;
-    return isLandscape ? 0.35 : 0.65;
-  }
-
-  double _getSecondaryTextSize(double screenWidth, double screenHeight) {
-    final smallerDimension =
-        screenWidth < screenHeight ? screenWidth : screenHeight;
-    if (smallerDimension < 300) return 14;
-    if (smallerDimension < 600) return 18;
-    if (smallerDimension < 1200) return 22;
-    if (smallerDimension < 2000) return 26;
-    return smallerDimension * 0.015;
-  }
-
   Widget _buildMainContent(BuildContext context, BoxConstraints constraints) {
     final screenWidth = constraints.maxWidth;
     final screenHeight = constraints.maxHeight;
     final isLandscape = screenWidth > screenHeight;
-    final smallerDimension =
-        screenWidth < screenHeight ? screenWidth : screenHeight;
+    final smallerDimension = screenWidth < screenHeight ? screenWidth : screenHeight;
 
-    final timeWidth =
-        screenWidth * _getResponsiveWidth(screenWidth, isLandscape);
-    final dateWeatherWidth =
-        screenWidth * (_getResponsiveWidth(screenWidth, isLandscape) * 0.9);
-    final timeTextSize = _getTimeTextSize(screenWidth, screenHeight);
-    final secondaryTextSize = _getSecondaryTextSize(screenWidth, screenHeight);
+    final timeTextSize = _getTimeTextSize(smallerDimension);
+    final secondaryTextSize = timeTextSize * 0.23;
 
-    final verticalSpacing = smallerDimension * (isLandscape ? 0.02 : 0.015);
-
-    Widget contentStack = Column(
+    final contentStack = Column(
       mainAxisAlignment: MainAxisAlignment.start,
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        SizedBox(height: verticalSpacing),
-        SizedBox(
-          width: timeWidth,
-          child: TimeWidget(
-            style: TextStyle(
-              fontSize: timeTextSize,
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-              letterSpacing: timeTextSize * 0.02,
-            ),
-            amPmStyle: TextStyle(
-              fontSize: timeTextSize *
-                  ((smallerDimension > 300 && isLandscape) ? 0.30 : 0.20),
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-            ),
+        SizedBox(height: smallerDimension * (isLandscape ? 0.02 : 0.015)),
+        TimeWidget(
+          style: TextStyle(
+            fontSize: timeTextSize,
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            letterSpacing: timeTextSize * 0.02,
+          ),
+          amPmStyle: TextStyle(
+            fontSize: timeTextSize * ((smallerDimension > 300 && isLandscape) ? 0.30 : 0.20),
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
           ),
         ),
-        SizedBox(
-          width: dateWeatherWidth,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Container(
-                decoration: SharedStyles.containerDecoration,
-                child: DateWidget(
-                  textStyle: TextStyle(
-                    fontSize: secondaryTextSize,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    height: 0.1,
-                  ),
-                ),
-              ),
-              MiniWeatherWidget(
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Container(
+              decoration: SharedStyles.containerDecoration,
+              child: DateWidget(
                 textStyle: TextStyle(
                   fontSize: secondaryTextSize,
                   color: Colors.white,
                   fontWeight: FontWeight.bold,
-                  height: 1.0,
+                  height: 0.1,
                 ),
               ),
-            ],
-          ),
+            ),
+            MiniWeatherWidget(
+              textStyle: TextStyle(
+                fontSize: secondaryTextSize,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                height: 1.0,
+              ),
+            ),
+          ],
         )
       ],
     );
-
-    Widget layoutWrapper = isLandscape
-        ? Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Expanded(
-                flex: 3,
-                child: contentStack,
-              ),
-              const Expanded(
-                flex: 2,
-                child: SizedBox(),
-              ),
-            ],
-          )
-        : Column(
-            children: [
-              Expanded(
-                flex: 3,
-                child: contentStack,
-              ),
-              const Expanded(
-                flex: 2,
-                child: SizedBox(),
-              ),
-            ],
-          );
 
     return Padding(
       padding: EdgeInsets.only(
         left: screenWidth * _getResponsivePadding(screenWidth),
         top: screenHeight * _getResponsivePadding(screenHeight, isWidth: false),
       ),
-      child: layoutWrapper,
+      child: isLandscape
+          ? Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Expanded(flex: 3, child: contentStack),
+                const Expanded(flex: 2, child: SizedBox()),
+              ],
+            )
+          : Column(
+              children: [
+                Expanded(flex: 3, child: contentStack),
+                const Expanded(flex: 2, child: SizedBox()),
+              ],
+            ),
     );
   }
 
@@ -318,9 +181,8 @@ void _checkAODStatus() async {
           body: Stack(
             children: [
               LayoutBuilder(
-                builder: (context, constraints) => SafeArea(
-                  child: _buildMainContent(context, constraints),
-                ),
+                builder: (context, constraints) => 
+                  SafeArea(child: _buildMainContent(context, constraints)),
               ),
               _buildCloseButton(),
             ],
@@ -332,7 +194,6 @@ void _checkAODStatus() async {
 
   @override
   void dispose() {
-    _animationController.dispose();
     _disableAODFeatures();
     super.dispose();
   }
--- lib/widgets/services/settings_service.dart
+++ lib/widgets/services/settings_service.dart
@@ -1,12 +1,33 @@
-// settings_service
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'dart:async';
 
+// Providers
 final settingsProvider = Provider((ref) => SettingsService());
 
 final aodEnabledProvider = StateProvider<bool>((ref) {
   final settings = ref.watch(settingsProvider);
   return settings.getSetting(SettingsService.aodEnabledKey, true) ?? true;
+});
+
+final timeFormatProvider = StateProvider<String>((ref) {
+  final settings = ref.watch(settingsProvider);
+  return settings.getSetting(SettingsService.timeFormatKey, '12') ?? '12';
+});
+
+final dateFormatProvider = StateProvider<String>((ref) {
+  final settings = ref.watch(settingsProvider);
+  return settings.getSetting(SettingsService.dateFormatKey, 'mon, 1 jan') ?? 'mon, 1 jan';
+});
+
+final temperatureUnitProvider = StateProvider<String>((ref) {
+  final settings = ref.watch(settingsProvider);
+  return settings.getSetting(SettingsService.temperatureUnitKey, 'celsius') ?? 'celsius';
+});
+
+final weatherUpdateFrequencyProvider = StateProvider<String>((ref) {
+  final settings = ref.watch(settingsProvider);
+  return settings.getSetting(SettingsService.weatherUpdateFrequencyKey, '30 minutes') ?? '30 minutes';
 });
 
 class SettingsService {
@@ -23,15 +44,37 @@ class SettingsService {
   static const String temperatureUnitKey = 'temperature_unit';
   static const String weatherLocationKey = 'weather_location';
   static const String weatherUpdateFrequencyKey = 'weather_update_frequency';
+  static const String isAutoLocationKey = 'weather_location_automatic';
 
   static final SettingsService _instance = SettingsService._internal();
   factory SettingsService() => _instance;
-  SettingsService._internal();
 
+  final _settingsController = StreamController<Map<String, dynamic>>.broadcast();
   late SharedPreferences _prefs;
+  
+  Stream<Map<String, dynamic>> get settingsStream => _settingsController.stream;
+
+  SettingsService._internal();
 
   Future<void> initialize() async {
     _prefs = await SharedPreferences.getInstance();
+    await _initializeDefaultSettings();
+    _notifyListeners();
+  }
+
+  Future<void> _initializeDefaultSettings() async {
+    if (!_prefs.containsKey(temperatureUnitKey)) {
+      await _prefs.setString(temperatureUnitKey, 'celsius');
+    }
+    if (!_prefs.containsKey(weatherUpdateFrequencyKey)) {
+      await _prefs.setString(weatherUpdateFrequencyKey, '30 minutes');
+    }
+    if (!_prefs.containsKey(weatherLocationKey)) {
+      await _prefs.setString(weatherLocationKey, 'Automatic');
+    }
+    if (!_prefs.containsKey(isAutoLocationKey)) {
+      await _prefs.setBool(isAutoLocationKey, true);
+    }
   }
 
   Future<void> setSetting(String key, dynamic value) async {
@@ -44,42 +87,63 @@ class SettingsService {
     } else if (value is double) {
       await _prefs.setDouble(key, value);
     }
+    _notifyListeners();
   }
 
   T? getSetting<T>(String key, T defaultValue) {
     return _prefs.get(key) as T? ?? defaultValue;
   }
 
-  // Weather location specific methods
+  // Weather specific methods
   Future<void> setWeatherLocation(String location, bool isAutomatic) async {
     await _prefs.setString(weatherLocationKey, location);
-    await _prefs.setBool('weather_location_automatic', isAutomatic);
+    await _prefs.setBool(isAutoLocationKey, isAutomatic);
+    _notifyListeners();
   }
 
-  String? getWeatherLocation() {
-    return _prefs.getString(weatherLocationKey);
+  String getWeatherLocation() {
+    return _prefs.getString(weatherLocationKey) ?? 'Automatic';
   }
 
   bool isWeatherLocationAutomatic() {
-    return _prefs.getBool('weather_location_automatic') ?? true;
+    return _prefs.getBool(isAutoLocationKey) ?? true;
   }
 
+  Future<Duration> getWeatherUpdateDuration() async {
+    final frequency = _prefs.getString(weatherUpdateFrequencyKey) ?? '30 minutes';
+    
+    switch (frequency) {
+      case '15 minutes':
+        return const Duration(minutes: 15);
+      case '1 hour':
+        return const Duration(hours: 1);
+      case '3 hours':
+        return const Duration(hours: 3);
+      default:
+        return const Duration(minutes: 30);
+    }
+  }
+
+  void _notifyListeners() {
+    _settingsController.add({
+      'temperature_unit': _prefs.getString(temperatureUnitKey),
+      'update_frequency': _prefs.getString(weatherUpdateFrequencyKey),
+      'weather_location': _prefs.getString(weatherLocationKey),
+      'is_auto_location': _prefs.getBool(isAutoLocationKey),
+      'theme': _prefs.getString(themeKey),
+      'language': _prefs.getString(languageKey),
+      'aod_enabled': _prefs.getBool(aodEnabledKey),
+      'time_format': _prefs.getString(timeFormatKey),
+      'date_format': _prefs.getString(dateFormatKey),
+    });
+  }
 
   Future<void> resetAllSettings() async {
-    final prefs = await SharedPreferences.getInstance();
-    await prefs.clear();
-    await initialize(); // Reinitialize with default values
+    await _prefs.clear();
+    await initialize();
+  }
+
+  void dispose() {
+    _settingsController.close();
   }
 }
-
-
-  final timeFormatProvider = StateProvider<String>((ref) {
-    final settings = ref.watch(settingsProvider);
-    return settings.getSetting(SettingsService.timeFormatKey, '12') ?? '12';
-  });
-
-// Create a provider for date format
-final dateFormatProvider = StateProvider<String>((ref) {
-  final settingsService = SettingsService();
-  return settingsService.getSetting(SettingsService.dateFormatKey, 'mon, 1 jan') ?? 'mon, 1 jan';
-});
